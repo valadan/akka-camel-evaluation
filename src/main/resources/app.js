395da2b7a181714b1431d94b17e3e9528c9fcca8
Robert Budźko <robert.krzysztof.budzko@gmail.com>
2015-08-23 18:25:50 +0200

    Client uses both sockets now.

--- src/main/resources/app.js
+++ src/main/resources/app.js
@@ -10,3 +10,16 @@ akkaWs.onopen = function (event) {
         )
     }
 };
+
+var camelWs = new WebSocket("ws://localhost:9292/camel");
+
+camelWs.onopen = function (event) {
+    for (var i = 0; i < 10; i++) {
+        setTimeout(
+            function () {
+                camelWs.send("Camel msg.");
+            },
+            i * 1000
+        )
+    }
+};
